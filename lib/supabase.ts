a07811ffe3a19dbd22c4eb39c8503805e55f1881
abid <abidwh273@gmail.com>
2026-06-19 20:07:03 +0700

    fix: pasang pengaman supabase biar netlify kaga rewel

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Kita ambil kuncinya, atau kasih kunci bohongan pas lagi proses 'build' di Netlify biar dia nggak nangis
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://pengaman-build.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'pengaman-key-123';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
